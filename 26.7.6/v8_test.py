e8a0d696f572879f34fbfded1a2bc6e3e661536c
littlewhite <littlewhite937@gmail.com>
2026-07-14 21:42:25 +0800

    加入訓練檔案與順練模型

--- 26.7.6/v8_test.py
+++ 26.7.6/v8_test.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 model.info()
 
-results = model(r"C:/Users/a/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0/LocalCache/local-packages/python312/site-packages/ultralytics/assets/bus.jpg", save=True)
+results = model(r"assets\training_files\L1.png", save=True)
 print(results)
